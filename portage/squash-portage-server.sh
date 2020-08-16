8ed27cf26964cc30c7c903de7588efcc8115cf3c
Quinn Jones <quinn_jones@pobox.com>
2020-08-16 12:03:24 -0400

    Add GPL verbiage to source

--- portage/squash-portage-server.sh
+++ portage/squash-portage-server.sh
@@ -1,8 +1,21 @@
 #!/bin/env bash
 
-# (c) Copyright 2020 by Quinn Jones, quinn_jones@pobox.com
-# All rights reserved.  The file named LICENSE specifies the terms
-# and conditions for redistribution.
+# Copyright (C) 2020 by Quinn Jones, quinn_jones@pobox.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA.
 #
 # Try executing with --help or --man for information
 
